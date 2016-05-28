281e1f79bfe0893c0c6a62795dc7fbdc4dffacd0
Wonderfall <wonderfall@schrodinger.io>
2016-05-28 17:16:47 +0200

    replace jade with pug

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ http.globalAgent.maxSockets = 30
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'public', 'images/favicon.png')))
 app.use(compression())
